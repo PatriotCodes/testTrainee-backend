a7ef70a9fde8e18d0f44994270fe12c698f1b961
AlexanderTriukhan <alexander.triukhan@devit-team.com>
2018-05-03 12:42:03 +0300

    test

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT']."db.php";
 
+echo $_SERVER['DOCUMENT_ROOT'];
+echo 'all user emails: ';
+echo PHP_EOL;
+
 $users = $conn->query('SELECT email from users');
 
 print_r($users);
-foreach ($users as $user) {
-	
-}
-
 
 echo 'ON!';
