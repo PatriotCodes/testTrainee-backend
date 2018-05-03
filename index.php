c5fa4543cbad47fdafd65c894f7c250fc14b48b2
AlexanderTriukhan <alexander.triukhan@devit-team.com>
2018-05-03 13:14:05 +0300

    ...

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ include "db.php";
 
 $users = $conn->query('SELECT email from users');
 
+if (isset($_POST['email']) && isset($_POST['password'])) {
+	
+}
+
 echo '<h2>all user emails: </h2>';
 while ($row = mysqli_fetch_array($users)) {
 	echo '<tr>';
