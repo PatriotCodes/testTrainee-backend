c09469aa7a6a0c95d3e556ec68c6ee2b62b1cc98
AlexanderTriukhan <alexander.triukhan@devit-team.com>
2018-05-03 12:25:04 +0300

    Checking connection

--- db.php
+++ db.php
@@ -11,4 +11,6 @@ $conn = new mysqli($host,$username,$password,$dbname);
 // Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
+} else {
+	echo ("Successfully connected!");
 }
