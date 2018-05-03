888a7a3ea0f9d78380714879fb5bdd50ae71ab99
AlexanderTriukhan <alexander.triukhan@devit-team.com>
2018-05-03 12:22:24 +0300

    Added database connection files

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+	"db" => array(
+		"db1" => array(
+			"dbname" => "test_db",
+			"username" => "patriot1306",
+			"password" => "hesoyam228",
+			"host" => "db4free.net"
+		)
+	)
+);
---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT']."config.php";
+
+$host = $config["db"]["db1"]["host"];
+$username = $config["db"]["db1"]["username"];
+$password = $config["db"]["db1"]["password"];
+$dbname = $config["db"]["db1"]["dbname"];
+
+$conn = new mysqli($host,$username,$password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
