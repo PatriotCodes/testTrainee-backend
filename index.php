cbe9315b4b667ba7388aa56c3af9fbe800fa5207
AlexanderTriukhan <alexander.triukhan@devit-team.com>
2018-05-03 13:06:16 +0300

    mysqli instead of mysql

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include "db.php";
 $users = $conn->query('SELECT email from users');
 
 echo '<h2>all user emails: </h2>';
-while ($row = mysql_fetch_array($users)) {
+while ($row = mysqli_fetch_array($users)) {
 	echo '<tr>';
 	foreach($row as $field) {
 		echo '<td>' . htmlspecialchars($field) . '</td>';
