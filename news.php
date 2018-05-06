f071ac9f07e108d2bf30de4bd1eacd0e43cca00a
PatriotCodes <patriot130690@gmail.com>
2018-05-06 14:50:06 +0300

    Echoing news

--- news.php
+++ news.php
@@ -3,7 +3,7 @@ include "db.php";
 
 $news = $conn->query('SELECT * from news');
 
-while ($row = mysqli_fetch_assoc($news)) {
-	echo "title:" . $row["title"];
-	echo "text:" . $row["text"];
+if (mysqli_num_rows($news) > 0) {
+	$JSON = json_encode(mysqli_fetch_assoc($news));
+	echo $JSON;
 }
