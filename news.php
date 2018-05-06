db031727aa74300c8cff21af83ac9fd1b9899593
PatriotCodes <patriot130690@gmail.com>
2018-05-06 14:52:45 +0300

    Echoing news

--- news.php
+++ news.php
@@ -5,7 +5,7 @@ $news = $conn->query('SELECT * from news');
 $news_array = Array();
 
 if (mysqli_num_rows($news) > 0) {
-	while($row = mysqli_fetch_assoc($result)) {
+	while($row = mysqli_fetch_assoc($news)) {
         array_push($news_array,$row);
     }
 	$JSON = json_encode(mysqli_fetch_assoc($news_array));
