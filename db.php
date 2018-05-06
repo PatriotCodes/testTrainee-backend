8607477eab86500823da35c82d25d957f936bc86
PatriotCodes <patriot130690@gmail.com>
2018-05-06 12:15:45 +0300

    removed successfully connected message

--- db.php
+++ db.php
@@ -11,6 +11,4 @@ $conn = new mysqli($host,$username,$password,$dbname);
 // Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
-} else {
-	echo ("Successfully connected!".PHP_EOL);
 }
