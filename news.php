6f683752a0158a3c010b9592cea30c1a58fca10b
PatriotCodes <patriot130690@gmail.com>
2018-05-07 01:28:40 +0300

    Echoing news

--- news.php
+++ news.php
@@ -5,7 +5,13 @@ $post = (array)json_decode(file_get_contents('php://input'));
 
 $id = 1;
 $news = $conn->query("SELECT * from news where id='$id'");
-	echo json_encode($news);
+$news_array = Array();
+	if (mysqli_num_rows($news) > 0) {
+		while($row = mysqli_fetch_assoc($news)) {
+			array_push($news_array,$row);
+		}
+		echo json_encode($news_array);
+	}
 	
 if (isset($post['id'])) {
 	$id = $post['id'];
