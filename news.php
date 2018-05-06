1b4b345d0ef3a58208a604386149d9dc7a2f8070
PatriotCodes <patriot130690@gmail.com>
2018-05-06 14:47:35 +0300

    Echoing news

--- news.php
+++ news.php
@@ -3,8 +3,7 @@ include "db.php";
 
 $news = $conn->query('SELECT * from news');
 
-while ($row = mysqli_fetch_array($news)) {
-	foreach($row as $field) {
-		print_r($field);
-	}
+while ($row = mysqli_fetch_assoc($news)) {
+	echo "title:" . $row["title"];
+	echo "text:" . $row["text"];
 }
