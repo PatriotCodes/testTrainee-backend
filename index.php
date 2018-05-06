56f956a5bd19e41ff82c5766b3bc26ab13a35717
PatriotCodes <patriot130690@gmail.com>
2018-05-06 12:35:19 +0300

    decoding json

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include "db.php";
 
 $users = $conn->query('SELECT email from users');
 
-$post = (array)json_decode(file_get_contents('php://input'););
+$post = (array)json_decode(file_get_contents('php://input'));
 
 if (isset($post['email']) && isset($post['password'])) {
 	while ($row = mysqli_fetch_array($users)) {
